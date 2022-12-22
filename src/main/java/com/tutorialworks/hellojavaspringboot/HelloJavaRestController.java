c19611cb53f4297aa959f7f6269594cfa7fd8485
onurkaratas <onkar.atasi@gmail.com>
2022-12-22 13:19:18 +0300

    update

--- src/main/java/com/tutorialworks/hellojavaspringboot/HelloJavaRestController.java
+++ src/main/java/com/tutorialworks/hellojavaspringboot/HelloJavaRestController.java
@@ -1,17 +1,14 @@
 package com.tutorialworks.hellojavaspringboot;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloJavaRestController {
 
-    @Value("${favourite.cheese}")
-    String favouriteCheese;
-
-    @GetMapping("/cheese")
+    @GetMapping("/index")
     public String getFavouriteCheese() {
-        return "My favourite cheese is:" + favouriteCheese;
+        return "Hello World";
     }
 }
